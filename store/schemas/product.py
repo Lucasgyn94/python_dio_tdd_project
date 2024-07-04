d8f2749c41bcaa01eec89cca3aa6b64feb75deac
Lucasgyn94 <needslucas944@gmail.com>
2024-07-03 23:31:32 -0300

    build v3 - Second passed schema

--- store/schemas/product.py
+++ store/schemas/product.py
@@ -2,8 +2,9 @@ from pydantic import Field
 from store.schemas.base import BaseSchemaMixin
 
 
+# OBS: ... indica ao pydentic que o valor precisa ser passado
 class ProductIn(BaseSchemaMixin):
-    name: str = Field(description="Product name")
-    quantity: int = Field(description="Product quantity")
-    price: float = Field(description="Product price")
-    status: bool = Field(description="Product status")
+    name: str = Field(..., description="Product name")
+    quantity: int = Field(..., description="Product quantity")
+    price: float = Field(..., description="Product price")
+    status: bool = Field(..., description="Product status")
--- tests/schemas/test_product.py
+++ tests/schemas/test_product.py
@@ -1,7 +1,28 @@
+from uuid import UUID
+
+from pydantic import ValidationError
+import pytest
 from store.schemas.product import ProductIn
 
 
 def test_schemas_validated():
-    product = ProductIn(name="Samsung S20", quantity=10, price=4.500, status=True)
+    data = {"name": "Samsung S20", "quantity": 10, "price": 4.500, "status": True}
+    #product = ProductIn(**data)
+    product = ProductIn.model_validate(data)
 
     assert product.name == "Samsung S20"
+    assert isinstance(product.id, UUID)
+
+def test_schemas_return_raise():
+    data = {"name": "Samsung S20", "quantity": 10, "price": 4.500}
+
+    with pytest.raises(ValidationError) as err:
+        ProductIn(**data)  # Instancia diretamente a classe ProductIn
+
+    assert err.value.errors()[0] == {
+        "type": "missing",
+        "loc": ("status",),
+        "msg": "Field required",
+        "input": {"name": "Samsung S20", "quantity": 10, "price": 4.5},
+        "url": "https://errors.pydantic.dev/2.8/v/missing"
+    }
