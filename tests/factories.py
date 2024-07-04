029df170715fd6ba08295977d2a22f13a12bde3a
Lucasgyn94 <needslucas944@gmail.com>
2024-07-04 00:37:40 -0300

    build v4 - third schema passed

---
+++ tests/factories.py
@@ -0,0 +1,2 @@
+def product_data():
+    return {"name": "Samsung S20", "quantity": 10, "price": 4.500, "status": True}
--- tests/schemas/test_product.py
+++ tests/schemas/test_product.py
@@ -3,21 +3,23 @@ from uuid import UUID
 from pydantic import ValidationError
 import pytest
 from store.schemas.product import ProductIn
+from tests.factories import product_data
 
 
 def test_schemas_validated():
-    data = {"name": "Samsung S20", "quantity": 10, "price": 4.500, "status": True}
+    data = product_data()
     #product = ProductIn(**data)
     product = ProductIn.model_validate(data)
 
     assert product.name == "Samsung S20"
     assert isinstance(product.id, UUID)
 
+
 def test_schemas_return_raise():
     data = {"name": "Samsung S20", "quantity": 10, "price": 4.500}
 
     with pytest.raises(ValidationError) as err:
-        ProductIn(**data)  # Instancia diretamente a classe ProductIn
+        ProductIn(**data)  
 
     assert err.value.errors()[0] == {
         "type": "missing",
